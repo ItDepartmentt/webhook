a61feb98ca0ff7450a0031308f7933305754b7a0
ItDepartmentt <97257335+ItDepartmentt@users.noreply.github.com>
2025-02-08 00:57:21 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,5 @@
-
+document.getElementById('domiciliario').addEventListener('change', function () {
+  const selected = this.value;
+  const reportContainer = document.getElementById('report-container');
+  reportContainer.innerHTML = `<p>Filtrando resultados para: ${selected}</p>`;
+});
